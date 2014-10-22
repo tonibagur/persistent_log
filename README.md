44cd36668665bbf3f262c5d9795ca16429991d38
Toni Bagur <toni.bagur@gmail.com>
2014-10-22 12:55:56 +0200

    Enhacements in the logging machinery

--- README.md
+++ README.md
@@ -1,10 +1,23 @@
+#PersistentLog for Python
+
 A simple persistent log backed by the ZODB object database. The logs are indexed by date and can be any serializable Python object:
 
-pl=persistent_log.PersistentLog('/home/test/test.fs')
-pl.log({'hello':'world'})
-pl.log({'good bye':'world'})
+```python
+pl=PersistentLog()
+a=pl.log_start({'hola':'mon'})
+b=pl.log_start({'hola3':'mon2'})
 for x in pl.root.log.keys():
-    print x , ":" , pl.root.log[x]
+    print x , ":-----------"
+    for y in pl.root.log[x]:
+        print y,pl.root.log_map[y]
+import time
+time.sleep(3)
+pl.log_stop(a)
+pl.log_stop(b)
+for x in pl.root.log.keys():
+    print x , ":-----------"
+    for y in pl.root.log[x]:
+        print y,pl.root.log_map[y]    
 pl.close()
-
+```
 
--- persistent_log.py
+++ persistent_log.py
@@ -1,5 +1,4 @@
 
-
 import persistent
 import transaction
 import ZODB
@@ -8,6 +7,9 @@ import BTrees.OOBTree
 import datetime
 
 
+import random
+
+import sys
 
 class PersistentLog(object):
     def __init__(self,db_path='log_db.fs'):
@@ -15,13 +17,36 @@ class PersistentLog(object):
         self.root = self.db.open().root()
         if not hasattr(self.root,'log'):
             self.root.log=BTrees.OOBTree.BTree()
+        if not hasattr(self.root,'log_map'):
+            self.root.log_map=BTrees.OOBTree.BTree()
 
-    def log(self,object_to_log):
+    def log_start(self,object_to_log):
         date_str=str(datetime.date.today())
+        now=datetime.datetime.now()
         if date_str not in self.root.log:
             self.root.log[date_str]=persistent.list.PersistentList()
-        self.root.log[date_str].append(object_to_log)
+        txid=self.get_txid()
+        self.root.log[date_str].append(txid)
+        self.root.log_map[txid]=persistent.mapping.PersistentMapping({'object':object_to_log,'start_time':now})
         transaction.commit()
+        return txid
+
+    def get_txid(self):
+        txid=None
+        while not txid:
+            temp=random.randint(0,sys.maxint)
+            if self.root.log_map.get(temp)==None:
+                txid=temp
+        return txid
+
+    def log_stop(self,txid):
+        if self.root.log_map.get(txid):
+            now=datetime.datetime.now()
+            elapsed=(now-self.root.log_map[txid]['start_time']).total_seconds()
+            self.root.log_map[txid]['stop_time']=now
+            self.root.log_map[txid]['elapsed']=elapsed
+            transaction.commit()
+
 
     def close(self):
         self.db.close()
@@ -30,11 +55,22 @@ class PersistentLog(object):
 
 if __name__=='__main__':
     pl=PersistentLog()
-    pl.log({'hola':'mon'})
-    pl.log({'hola3':'mon2'})
+    a=pl.log_start({'hola':'mon'})
+    b=pl.log_start({'hola3':'mon2'})
     for x in pl.root.log.keys():
-        print x , ":" , pl.root.log[x]
+        print x , ":-----------"
+        for y in pl.root.log[x]:
+            print y,pl.root.log_map[y]
+    import time
+    time.sleep(3)
+    pl.log_stop(a)
+    pl.log_stop(b)
+    for x in pl.root.log.keys():
+        print x , ":-----------"
+        for y in pl.root.log[x]:
+            print y,pl.root.log_map[y]    
     pl.close()
+    
 
         
 
